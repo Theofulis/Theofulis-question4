503919ec78ee8e387fb8020602a1b72b7151ce0f
Theofulis <philusmashego@gmail.com>
2020-07-17 14:20:31 +0200

    This code counts the number of strings

---
+++ question_4/src/question_4/Question_4.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package question_4;
+
+/**
+ *
+ * @author theophilus
+ */
+public class Question_4 {
+
+   
+    static final int intial = 0; 
+    static final int output = 1; 
+    
+    public static void main(String args[]) 
+    { 
+        String str = " besides skills, responsibility is equally important"; 
+        System.out.println("Number of words counted : " + countNumberofWords(str)); 
+    }    
+    static int countNumberofWords(String str) 
+    { 
+        int state = output; 
+        int numberOfWords = 0;  
+        int j = 0; 
+          
+       
+        while (j < str.length()) 
+        { 
+             
+            if (str.charAt(j) == ' ' || str.charAt(j) == '\n' 
+                    || str.charAt(j) == '\t') 
+                state = output; 
+                  
+       
+            
+            else if (state == output) 
+            { 
+                state = intial; 
+                ++numberOfWords; 
+            } 
+            ++j; 
+        } 
+        return numberOfWords; 
+    }       
+} 
+
+    
+
